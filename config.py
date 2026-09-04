fe81153d21344537306b2fdc85e53456c00586b2
kobramethod-cyber <kobramethod@gmail.com>
2026-09-04 17:51:22 +0530

    Create config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+BOT_TOKEN = "8639243087:AAHQgRU5Et70xK76RGI57c88-Bvy0ELI6gg"
+
+MONGO_URI = "mongodb+srv://Kobra:Kartik9307@cluster0.oxqflcj.mongodb.net/premium_bot?retryWrites=true&w=majority"
+
+ADMINS = [
+    1936430807,
+    8720701910
+]
+
+UPI_ID = "nagargoje12@ptyes"
+
+DB_NAME = "MONGO_URI"
